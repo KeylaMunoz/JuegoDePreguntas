bd55f42ba46e68ffeae2835a3189072f2151bf17
Keyla Muñoz <keyla.keyla.munoz@gmail.com>
2024-02-10 21:12:06 -0300

    creacion de funciones

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
+//ingreso de usuario
 let usuario = prompt ("Ingrese nombre de usuario:");
 let puntos = 0;
 
 while (usuario ===""){
-    alert ("Para comenzar, ingresa tu nombre:");   
+    alert ("Para comenzar, ingresa tu nombre:");
     usuario = prompt ("Ingrese nombre de usuario:");
 }
 
 alert("¡Bienvenido/a " + usuario + " a este juego de preguntas!")
 alert("¿Comenzamos?🎯")
 
-
+// preguntas
 let pregunta1 = parseInt(prompt("¿Cuales son los colores primarios? \nElije la opción correcta: \n1. Amarillo, Rojo y Verde \n2. Azul, Rojo y Amarillo  \n3. Amarillo, Azul y Violeta"));
 pregunta1 = parseInt(pregunta1);
 
@@ -30,7 +31,7 @@ let pregunta2 = prompt("¿Qué elemento principal necesitan las plantas para sob
 pregunta2 = pregunta2.toLowerCase();
 while (!isNaN(pregunta2) || pregunta2 === "") {
     alert("⚠️Por favor, ingresa una opción válida.");
-    
+
     pregunta2 = prompt("¿Qué elemento principal necesitan las plantas para sobrevivir?");
     pregunta2 = pregunta2.toLowerCase();
 }
@@ -46,7 +47,7 @@ let pregunta3 = prompt("¿Cuál de estos animales es invertebrado? \n1. Serpient
 pregunta3 = pregunta3.toLowerCase();
 while (!isNaN(pregunta3) || pregunta3 === "") {
     alert("⚠️Por favor, ingresa una opción válida.");
-    
+
     pregunta3 = prompt("¿Cuál de estos animales es invertebrado? \n1. Serpiente \n2. Tortuga \n3. Caracol");
     pregunta3 = pregunta3.toLowerCase();
 }
@@ -62,7 +63,7 @@ let pregunta4 = (prompt("¿Cuántos meses del año comienzan con la letra M?"));
 pregunta4 = parseInt(pregunta4);
 while (isNaN(pregunta4) || pregunta4 === "") {
     alert("⚠️Por favor, ingresa una opción válida.");
-    
+
     pregunta4 = parseInt(prompt("¿Cuántos meses del año comienzan con la letra M?"));
 }
 if (pregunta4 == 2){
@@ -77,7 +78,7 @@ let pregunta5 = (prompt("¿Cuantos planetas conforman nuestro Sistema Solar?"));
 pregunta5 = parseInt(pregunta5);
 while (isNaN(pregunta5) || pregunta5 === "") {
     alert("⚠️Por favor, ingresa una opción válida.");
-    
+
     pregunta5 = parseInt(prompt("¿Cuantos planetas conforman nuestro Sistema Solar?"));
 }
 if (pregunta5 == 8){
@@ -92,7 +93,7 @@ let pregunta6 = (prompt("Se retiró el aire de una cámara y se dejó caer de la
 pregunta6 = parseInt(pregunta6);
 while (isNaN(pregunta6) || pregunta6 < 1 || pregunta6 > 3 || pregunta6 === "") {
     alert("⚠️Por favor, ingresa una opción válida.");
-    
+
     pregunta6 = parseInt(prompt("Se retiró el aire de una cámara y se dejó caer de la misma altura y al mismo tiempo una moneda y una pluma ¿Cuál cae primero? \nElije la opción correcta: \n1. La moneda \n2. La pluma \n3. Ambas caen al mismo tiempo"));
 }
 if (pregunta6 == 3){
@@ -102,17 +103,19 @@ if (pregunta6 == 3){
     alert("Respuesta incorrecta 🚨");
 }
 
-
-switch (true) {
-    case puntos <= 3:
-    alert("Has obtenido 🌟 estrella \n¡Oh no! Tu puntaje es regular 😬");
-    break;
-    case puntos <= 5:
-    alert("Has obtenido 🌟🌟 estrellas \n¡Lo has hecho bien!✌️");
-    break;
-    default:
-    alert("Has obtenido 🌟🌟🌟 estrellas \n¡¡FELICIDADES!! LO HAS HECHO EXCELENTE🏆");
-    break;
+// puntuacion final del usuario
+function puntaje (jugador){
+    switch (true) {
+        case puntos <= 3:
+        alert(jugador + " has obtenido 🌟 estrella \n¡Oh no! Tu puntaje es regular 😬");
+        break;
+        case puntos <= 5:
+        alert(jugador + " has obtenido 🌟🌟 estrellas \n¡Lo has hecho bien!✌️");
+        break;
+        default:
+        alert(jugador + " has obtenido 🌟🌟🌟 estrellas \n¡¡FELICIDADES!! LO HAS HECHO EXCELENTE🏆");
+        break;
+    }
 }
 
-
+puntaje(usuario)
