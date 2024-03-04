7bcc40a9fd38beaeb5d5b2bf7700673b25865ee7
Keyla Muñoz <keyla.keyla.munoz@gmail.com>
2024-03-04 15:59:06 -0300

    modificacion de codigo con agregadode array y objetos

--- js/main.js
+++ js/main.js
@@ -1,121 +1,71 @@
 //ingreso de usuario
-let usuario = prompt ("Ingrese nombre de usuario:");
-let puntos = 0;
+const datosDeUsuario = [];
 
-while (usuario ===""){
-    alert ("Para comenzar, ingresa tu nombre:");
-    usuario = prompt ("Ingrese nombre de usuario:");
+function guardarUsuario (){
+    let nombre = prompt ("Ingrese nombre de usuario:");
+    let email = prompt ("Ingrese un email");
+    let usuario = prompt ("Ingrese un nombre de usuaraio");
+     
+    let nuevoUsuario = {
+        nombre: nombre,
+        email: email,
+        usuario: usuario
+        };
+        
+    datosDeUsuario.push(nuevoUsuario);
+    return nuevoUsuario;
+}
+let usuarioGuardado = guardarUsuario();    
+let nombreDeUsuario = usuarioGuardado.nombre;    
+alert(`¡Bienvenido/a " ${nombreDeUsuario} a este juego de preguntas! \nComencemos!!🚀`);
+    
+//Preguntas NIVEL 1
+
+let puntajes = {
+    nivel1: 0,
+    nivel2: 0
+};
+
+function RespuestaCorrecta(puntajes, nivel, pregunta, respuestaCorrecta, respuestaJustificada){
+    let respuestaIngresada = parseInt(prompt(pregunta));
+
+    while (isNaN(respuestaIngresada) || respuestaIngresada < 1 || respuestaIngresada > 3 || respuestaIngresada === "") {
+        alert("⚠️Por favor, ingresa una opción válida.");
+        respuestaIngresada = parseInt(prompt(pregunta));
+    };
+
+    if (respuestaIngresada === respuestaCorrecta) {
+        alert ("🎉¡Respuesta correcta!🎉");
+        return puntajes[nivel]++;
+    }else {
+        alert (`Respuesta incorrecta 🚨 ${respuestaJustificada}`);
+    };
 }
 
-alert("¡Bienvenido/a " + usuario + " a este juego de preguntas!")
-alert("¿Comenzamos?🎯")
-
-// preguntas
-let pregunta1 = parseInt(prompt("¿Cuales son los colores primarios? \nElije la opción correcta: \n1. Amarillo, Rojo y Verde \n2. Azul, Rojo y Amarillo  \n3. Amarillo, Azul y Violeta"));
-pregunta1 = parseInt(pregunta1);
-
-while (isNaN(pregunta1) || pregunta1 < 1 || pregunta1 > 3 || pregunta1 === "") {
-    alert("⚠️Por favor, ingresa una opción válida.");
-
-    pregunta1 = prompt("¿Cuales son los colores primarios? \nElije la opción correcta: \n1. Amarillo, Rojo y Verde \n2. Azul, Rojo y Amarillo  \n3. Amarillo, Azul y Violeta");
+// Puntuacion del usuario
+function Puntaje (puntaje, nivel ){
+    if (puntaje <= 1){
+        alert(`NIVEL ${nivel}  \n ${nombreDeUsuario} has obtenido 🌟 estrella \n¡Oh no! Tu puntaje es regular 😬`);
+    } else if (puntaje <= 2){
+        alert(`NIVEL ${nivel}  \n ${nombreDeUsuario} has obtenido 🌟🌟 estrellas \n¡Lo has hecho bien!✌️`);
+    } else{
+        alert(`NIVEL ${nivel}  \n ${nombreDeUsuario} has obtenido 🌟🌟🌟 estrellas \n¡¡FELICIDADES!! HAS LOGRADO LA MEJOR PUNTUACIÓN 🏆`);
+    };
 }
-if (pregunta1 == 2){
-    alert("🎉¡Respuesta correcta!🎉");
-    puntos++;
-} else {
-    alert("Respuesta incorrecta 🚨");
-}
+let pregunta1 = new RespuestaCorrecta (puntajes, 'nivel1', "¿Cuales son los colores primarios? \n1- Amarillo, Rojo y Verde \n2- Azul, Rojo y Amarillo  \n3- Amarillo, Azul y Violeta", 2, "\nLos colores primarios son el azul, rojo y amarillo");
+let pregunta2 = new RespuestaCorrecta (puntajes, 'nivel1', "¿Qué elemento principal necesitan las plantas para sobrevivir? \n1- Agua \n2- Sombra \n3- Tierra", 1, "\nLas plantas no pueden sobrevivir sin agua");
+let pregunta3 = new RespuestaCorrecta (puntajes, 'nivel1', "¿Cuál de estos animales es invertebrado? \n1- Serpiente \n2- Tortuga \n3- Caracol", 3, "\nLos caracoles son invertebrados");
 
 
-let pregunta2 = prompt("¿Qué elemento principal necesitan las plantas para sobrevivir?");
-pregunta2 = pregunta2.toLowerCase();
-while (!isNaN(pregunta2) || pregunta2 === "") {
-    alert("⚠️Por favor, ingresa una opción válida.");
+Puntaje(puntajes.nivel1,"1");
 
-    pregunta2 = prompt("¿Qué elemento principal necesitan las plantas para sobrevivir?");
-    pregunta2 = pregunta2.toLowerCase();
-}
-if (pregunta2 == "agua"){
-    alert("🎉¡Respuesta correcta!🎉");
-    puntos++;
-} else {
-    alert("Respuesta incorrecta 🚨");
-}
+//Preguntas NIVEL 2
+alert("NIVEL 2 \n=¿Estas listo? ¡Comencemos! 🚀")
+let puntosNivel2 = 0;
 
+let pregunta4 = new RespuestaCorrecta (puntajes, 'nivel2', "¿Cuántos meses del año comienzan con la letra M? \n1- Tres \n2- Cinco \n3- Dos", 3, "\nSon dos: Marzo y Mayo");
+let pregunta5 = new RespuestaCorrecta (puntajes, 'nivel2', "¿Cuantos planetas conforman nuestro Sistema Solar? \n1- Diez \n2- Ocho \n3- Nueve", 2, "\nSon 8: Mercurio, Venus, Tierra, Marte, Júpiter, Saturno, Urano y Neptuno.");
+let pregunta6 = new RespuestaCorrecta (puntajes, 'nivel2', "Se retiró el aire de una cámara y se dejó caer de la misma altura y al mismo tiempo una moneda y una pluma ¿Cuál cae primero? \nElije la opción correcta: \n1- La moneda \n2- La pluma \n3- Ambas caen al mismo tiempo", 3, "\nLa gravedad actúa en ambos cuerpos por igual. Entonces al sacar el aire de la cámara, la pluma ya no presenta resistencia.");
 
-let pregunta3 = prompt("¿Cuál de estos animales es invertebrado? \n1. Serpiente \n2. Tortuga \n3. Caracol");
-pregunta3 = pregunta3.toLowerCase();
-while (!isNaN(pregunta3) || pregunta3 === "") {
-    alert("⚠️Por favor, ingresa una opción válida.");
+Puntaje(puntajes.nivel2,"2");
 
-    pregunta3 = prompt("¿Cuál de estos animales es invertebrado? \n1. Serpiente \n2. Tortuga \n3. Caracol");
-    pregunta3 = pregunta3.toLowerCase();
-}
-if (pregunta3 == "caracol"){
-    alert("¡¡WOW MUY BIEN!!😎 \n¡Al parecer, eres una persona muy inteligente! \n¿Continuamos?");
-    puntos++;
-} else {
-    alert("Respuesta incorrecta🚨");
-}
-
-
-let pregunta4 = (prompt("¿Cuántos meses del año comienzan con la letra M?"));
-pregunta4 = parseInt(pregunta4);
-while (isNaN(pregunta4) || pregunta4 === "") {
-    alert("⚠️Por favor, ingresa una opción válida.");
-
-    pregunta4 = parseInt(prompt("¿Cuántos meses del año comienzan con la letra M?"));
-}
-if (pregunta4 == 2){
-    alert("🎉¡Respuesta correcta!🎉");
-    puntos++;
-} else {
-    alert("Respuesta incorrecta🚨");
-}
-
-
-let pregunta5 = (prompt("¿Cuantos planetas conforman nuestro Sistema Solar?"));
-pregunta5 = parseInt(pregunta5);
-while (isNaN(pregunta5) || pregunta5 === "") {
-    alert("⚠️Por favor, ingresa una opción válida.");
-
-    pregunta5 = parseInt(prompt("¿Cuantos planetas conforman nuestro Sistema Solar?"));
-}
-if (pregunta5 == 8){
-    alert("🎉¡Correcto!🎉 \n Ahora vamos a aumentar un poco la dificultad...🔥");
-    puntos++;
-} else {
-    alert("Respuesta incorrecta🚨");
-}
-
-
-let pregunta6 = (prompt("Se retiró el aire de una cámara y se dejó caer de la misma altura y al mismo tiempo una moneda y una pluma ¿Cuál cae primero? \nElije la opción correcta: \n1. La moneda \n2. La pluma \n3. Ambas caen al mismo tiempo"));
-pregunta6 = parseInt(pregunta6);
-while (isNaN(pregunta6) || pregunta6 < 1 || pregunta6 > 3 || pregunta6 === "") {
-    alert("⚠️Por favor, ingresa una opción válida.");
-
-    pregunta6 = parseInt(prompt("Se retiró el aire de una cámara y se dejó caer de la misma altura y al mismo tiempo una moneda y una pluma ¿Cuál cae primero? \nElije la opción correcta: \n1. La moneda \n2. La pluma \n3. Ambas caen al mismo tiempo"));
-}
-if (pregunta6 == 3){
-    alert("🎉¡¡CORRECTO!!🎉 \n¡Tienes una mente muy brillante!👏👏");
-    puntos++;
-} else {
-    alert("Respuesta incorrecta 🚨");
-}
-
-// puntuacion final del usuario
-function puntaje (jugador){
-    switch (true) {
-        case puntos <= 3:
-        alert(jugador + " has obtenido 🌟 estrella \n¡Oh no! Tu puntaje es regular 😬");
-        break;
-        case puntos <= 5:
-        alert(jugador + " has obtenido 🌟🌟 estrellas \n¡Lo has hecho bien!✌️");
-        break;
-        default:
-        alert(jugador + " has obtenido 🌟🌟🌟 estrellas \n¡¡FELICIDADES!! LO HAS HECHO EXCELENTE🏆");
-        break;
-    }
-}
-
-puntaje(usuario)
